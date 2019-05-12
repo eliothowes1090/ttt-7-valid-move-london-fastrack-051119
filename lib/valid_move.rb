9d1f4c8fef50e7a56e0b6e3c5042a699ee66d9ce
Eliot Howes <ejhowes@gmail.com>
2019-05-12 06:55:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -18,13 +18,3 @@ def position_taken?(board, index)
   taken
 end
 
-
-def valid_move?(board, position)
-  if position.to_i.between?(1,9)
-    if !position_taken?(board, position.to_i-1)
-      true
-    end
-  end
-
-  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-end
